44d976a4594cfb33b91eae4a61cfb5e7a60368d1
Wei Hsun <drunkhsun@gmail.com>
2019-08-27 10:28:28 +0800

    公版完成

--- pbook_index/example_index.php
+++ pbook_index/example_index.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/__html_head.php' ?>
 <style>
-    body{
+    body {
         background: url(../images/bg.png) repeat center top;
     }
 </style>
@@ -205,134 +205,8 @@
     </aside>
 
     <!-- 右邊section資料欄位 -->
-    <section>
-        <div class="container">
-            <nav class="navbar justify-content-between" style="padding: 0px;width: 80vw;">
-                <div>
-                    <h4>廠商總表</h4>
-                    <div class="title_line"></div>
-                </div>
-                <ul class="nav justify-content-between">
-                    <li class="nav-item">
-                        <div style="padding: 0.375rem 0.75rem;">
-                            <i class="fas fa-check"></i>
-                            目前總計___筆資料
-                        </div>
-                    </li>
-                    <li class="nav-item" style="margin: 0px 10px">
-                        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
-                            <i class="fas fa-plus-circle"></i>
-                            新增廠商
-                        </button>
-                    </li>
-                    <li class="nav-item" style="flex-grow: 1">
-                        <form class="form-inline my-2 my-lg-0">
-                            <input class="search form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </form>
-                    </li>
-                </ul>
-            </nav>
-
-            <!-- 每個人填資料的區塊 -->
-            <div style="margin-top: 1rem">
-                <table class="table table-striped table-bordered" style="text-align: center">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">姓名</th>
-                            <th scope="col">電子郵箱</th>
-                            <th scope="col">手機</th>
-                            <th scope="col">生日</th>
-                            <th scope="col">地址</th>
-                            <th scope="col">修改</th>
-                            <th scope="col">刪除</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>王小明</td>
-                            <td>hihi@gmail.com</td>
-                            <td>0988555888</td>
-                            <td>2000-05-09</td>
-                            <td>台北市大安區資訊教育研究所</td>
-                            <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    </tbody>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>王小明</td>
-                            <td>hihi@gmail.com</td>
-                            <td>0988555888</td>
-                            <td>2000-05-09</td>
-                            <td>台北市大安區資訊教育研究所</td>
-                            <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    </tbody>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>王小明</td>
-                            <td>hihi@gmail.com</td>
-                            <td>0988555888</td>
-                            <td>2000-05-09</td>
-                            <td>台北市大安區資訊教育研究所</td>
-                            <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    </tbody>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>王小明</td>
-                            <td>hihi@gmail.com</td>
-                            <td>0988555888</td>
-                            <td>2000-05-09</td>
-                            <td>台北市大安區資訊教育研究所</td>
-                            <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <!-- 我是分頁按鈕列 請自取並調整頁面擺放位置 -->
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item">
-                        <a class="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                        <a class="page-link my_text_blacktea" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-
-            <!-- 刪除提示框 -->
-            <!-- <div class="delete update card">
-                    <div class="delete card-body">
-                        <label class="delete_text">您確認要刪除資料嗎?</label>
-                        <div>
-                            <button type="button" class="delete btn btn-danger">確認</button>
-                            <button type="button" class="delete btn btn-warning">取消</button>
-                        </div>
-                    </div>
-                </div> -->
-
+    <section style="width: calc(100vw - 280px);">
+        <img src="../images/admin_bg.png" alt="" style="height: 100%;object-fit: contain;width:100%">
     </section>
 </div>
 <?php include __DIR__ . '/__html_foot.php' ?>
--- pbook_index/example_update.php
+++ pbook_index/example_update.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/__html_head.php' ?>
 <style>
-    body{
+    body {
         background: url(../images/bg.png) repeat center top;
     }
 </style>
@@ -204,135 +204,62 @@
         </div>
     </aside>
 
+
     <!-- 右邊section資料欄位 -->
     <section>
         <div class="container">
             <nav class="navbar justify-content-between" style="padding: 0px;width: 80vw;">
                 <div>
-                    <h4>廠商總表</h4>
+                    <h4>會員資料修改</h4>
                     <div class="title_line"></div>
                 </div>
-                <ul class="nav justify-content-between">
-                    <li class="nav-item">
-                        <div style="padding: 0.375rem 0.75rem;">
-                            <i class="fas fa-check"></i>
-                            目前總計___筆資料
-                        </div>
-                    </li>
-                    <li class="nav-item" style="margin: 0px 10px">
-                        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
-                            <i class="fas fa-plus-circle"></i>
-                            新增廠商
-                        </button>
-                    </li>
-                    <li class="nav-item" style="flex-grow: 1">
-                        <form class="form-inline my-2 my-lg-0">
-                            <input class="search form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </form>
-                    </li>
-                </ul>
             </nav>
 
             <!-- 每個人填資料的區塊 -->
-            <div style="margin-top: 1rem">
-                <table class="table table-striped table-bordered" style="text-align: center">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">姓名</th>
-                            <th scope="col">電子郵箱</th>
-                            <th scope="col">手機</th>
-                            <th scope="col">生日</th>
-                            <th scope="col">地址</th>
-                            <th scope="col">修改</th>
-                            <th scope="col">刪除</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>王小明</td>
-                            <td>hihi@gmail.com</td>
-                            <td>0988555888</td>
-                            <td>2000-05-09</td>
-                            <td>台北市大安區資訊教育研究所</td>
-                            <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    </tbody>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>王小明</td>
-                            <td>hihi@gmail.com</td>
-                            <td>0988555888</td>
-                            <td>2000-05-09</td>
-                            <td>台北市大安區資訊教育研究所</td>
-                            <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    </tbody>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>王小明</td>
-                            <td>hihi@gmail.com</td>
-                            <td>0988555888</td>
-                            <td>2000-05-09</td>
-                            <td>台北市大安區資訊教育研究所</td>
-                            <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    </tbody>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>王小明</td>
-                            <td>hihi@gmail.com</td>
-                            <td>0988555888</td>
-                            <td>2000-05-09</td>
-                            <td>台北市大安區資訊教育研究所</td>
-                            <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <!-- 我是分頁按鈕列 請自取並調整頁面擺放位置 -->
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item">
-                        <a class="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                        <a class="page-link my_text_blacktea" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-
-            <!-- 刪除提示框 -->
-            <!-- <div class="delete update card">
-                    <div class="delete card-body">
-                        <label class="delete_text">您確認要刪除資料嗎?</label>
-                        <div>
-                            <button type="button" class="delete btn btn-danger">確認</button>
-                            <button type="button" class="delete btn btn-warning">取消</button>
-                        </div>
+            <div class="container">
+                <div class="update card mx-auto">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="name" class="update_label">姓名</label>
+                                <input type="text" class="update form-control" id="name" name="name">
+                                <small id="nameHelp" class="update form-text">示意:錯誤顯示訊息</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="email" class="update_label">電子郵箱</label>
+                                <input type="text" class="update form-control" id="email" name="email">
+                                <small id="emailHelp" class="update form-text">示意:錯誤顯示訊息</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="mobile" class="update_label">手機</label>
+                                <input type="text" class="update form-control" id="mobile" name="mobile">
+                                <small id="mobileHelp" class="update form-text">示意:錯誤顯示訊息</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="birthday" class="update_label">生日</label>
+                                <input type="text" class="update form-control" id="birthday" name="birthday">
+                                <small id="birthdayHelp" class="update form-text">示意:錯誤顯示訊息</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="address" class="update_label">地址</label>
+                                <input type="text" class="update form-control" id="address" name="address">
+                                <small id="addressHelp" class="update form-text">示意:錯誤顯示訊息</small>
+                            </div>
+                            <div style="text-align: center">
+                                <button type="submit" class="btn btn-warning" id="submit_btn">&nbsp;確&nbsp;認&nbsp;修&nbsp;改&nbsp;</button>
+                            </div>
+                        </form>
                     </div>
-                </div> -->
+                </div>
 
+                <!-- 以下為修改或新增成功才會跳出來的顯示框 -->
+                <!-- <div class="success update card">
+                        <div class="success card-body">
+                            <label class="success_text">修改成功</label>
+                            <div><img class="success_img" src="../images/icon_checked.svg"></div>
+                        </div>
+                    </div> -->
+            </div>
     </section>
 </div>
 <?php include __DIR__ . '/__html_foot.php' ?>
