5c37e4d0afe3a0691e52073906825b85c0697195
Wei Hsun <drunkhsun@gmail.com>
2019-08-27 09:52:37 +0800

    公版

---
+++ pbook_index/__html_head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <script src="../lib/jquery-3.4.1.js"></script>
+    <script src="../bootstrap/js/bootstrap.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../fontawesome/css/all.css">
+    <link rel="stylesheet" href="../lib/mycss.css">
+    <title><?= isset($page_title) ? $page_title : 'Document'  ?></title>
+</head>
+<body>
