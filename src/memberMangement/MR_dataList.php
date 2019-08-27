766eef7098f2f70e998ab3b160dd05df0ce7b3de
Ruby <ruby0771134@gmail.com>
2019-08-27 11:20:53 +0800

    1109

---
+++ src/memberMangement/MR_dataList.php
@@ -0,0 +1,36 @@
+<?php require  __DIR__ . '/_db_connect.php' ?>
+
+<?php include '/../../pbook_index/__html_head.php' ?>
+<?php include '/../../pbook_index/__html_head.php' ?>
+
+<div style="margin-top: 1rem">
+    <table class="table table-striped table-bordered" style="text-align: center">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">姓名</th>
+                <th scope="col">電子郵箱</th>
+                <th scope="col">手機</th>
+                <th scope="col">生日</th>
+                <th scope="col">地址</th>
+                <th scope="col">修改</th>
+                <th scope="col">刪除</th>
+            </tr>
+        </thead>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+    </table>
+</div>
+
+
+
+
+
+
+
+
+<?php include __DIR__ . '/_footer.php' ?>
---
+++ src/memberMangement/_index.php
@@ -0,0 +1,12 @@
+<?php include __DIR__ . '/_header.php' ?>
+<?php include __DIR__ . '/_navBar.php' ?>
+
+
+
+
+
+
+
+
+
+<?php include __DIR__ . '/_footer.php' ?>
