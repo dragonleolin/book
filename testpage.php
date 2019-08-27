89264a62139e9e6e91c0108a01b3d78b7bdd8672
Shan Yi <a24215188@gmail.com>
2019-08-27 10:19:05 +0800

    0827

---
+++ testpage.php
@@ -0,0 +1,13 @@
+<?php require '___BR_connect_db.php'; 
+$page_name = 'BR_datalist';
+$page_title = '書評人資料總表';
+?>
+<?php require '__BR_head.php'; ?>
+
+
+<div class="container">
+    <?php require '__BR_nav.php'; ?>
+
+</div>
+
+<?php require '__BR_foot.php'; ?>
