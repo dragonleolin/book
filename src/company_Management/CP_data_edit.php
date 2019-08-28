aa0d520c05212701aabd3c15944f8fc34ff332c4
Wei Hsun <drunkhsun@gmail.com>
2019-08-28 15:14:00 +0800

    wei husn

--- src/company_Management/CP_data_edit.php
+++ src/company_Management/CP_data_edit.php
@@ -1,7 +1,19 @@
 <?php
 // require __DIR__. '/__admin_required.php';
 require __DIR__ . '/__connect_db.php';
-$page_title = '新增出版社';
+$page_title = '資料修改';
+
+$sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
+if (empty($sid)) {
+    header('Location: CP_data_list.php');
+    exit;
+}
+$sql = "SELECT * FROM `cp_data_list` WHERE `sid` = $sid";
+$row = $pdo->query($sql)->fetch();
+if (empty($row)) {
+    header('Location: CP_data_list.php');
+    exit;
+}
 $form_data1 = [
     '出版社名' => 'cp_name',
     '聯絡人' => 'cp_contact_p',
@@ -16,7 +28,14 @@ $form_data2 = [
     '密碼' => 'cp_password',
     'logo' => 'cp_logo',
 ];
+
+$seq = "SELECT  1 + (SELECT count(*) FROM `cp_data_list` where `sid` < $sid)  FROM `cp_data_list` limit 1"; //拿到第幾筆
+$stmt2 = $pdo->query($seq)->fetch();
+foreach ($stmt2 as $k => $v) {
+    $previous_page = intval($v / 8) + 1;
+}
 ?>
+
 <?php include __DIR__ . '/../../pbook_index/__html_head.php' ?>
 <style>
     body {
@@ -27,10 +46,6 @@ $form_data2 = [
         left: calc(50% - 350px);
         top: 30%;
     }
-    .rand_button{
-        right :10%  ;
-        top : 0;  
-    }
 </style>
 <?php include __DIR__ . '/../../pbook_index/__html_body.php' ?>
 <?php include __DIR__ . '/../../pbook_index/__navbar.php' ?>
@@ -39,27 +54,25 @@ $form_data2 = [
     <div class="container">
         <nav class="navbar justify-content-between" style="padding: 0px;width: 80vw;">
             <div>
-                <h4>新增出版社</h4>
+                <h4>資料修改</h4>
                 <div class="title_line"></div>
             </div>
         </nav>
         <!-- 每個人填資料的區塊 -->
         <div class="container position-relative" style="margin-left:calc( 50% - 314px)">
-            <div style="text-align: center">
-                <button type="button" class="btn btn-warning position-absolute rand_button" onclick="rand_data()">隨機產生</button>
-            </div>
             <div class="row">
                 <form name="form1" style="width:1000px" onsubmit="return checkForm()">
+                    <input type="hidden" name="sid" value="<?= $row['sid'] ?>">
                     <div class="form-group d-flex">
                         <div class="container">
                             <?php foreach ($form_data1 as $k => $v) : ?>
                             <label for="<?= $v ?>" class="update_label pt-3"><?= $k ?></label>
                             <?php if ($v == 'cp_password') { ?>
                             <!-- 密碼用password type -->
-                            <input type="password" class="update form-control" id="cp_password" name="cp_password" autocomplete="new-password">
+                            <input type="password" class="update form-control" id="cp_password" name="cp_password" autocomplete="new-password" value="<?= htmlentities($row[$v]) ?>">
                             <small id="cp_passwordHelp" class="update form-text"></small>
                             <?php } else { ?>
-                            <input type="text" class="update form-control" id="<?= $v ?>" name="<?= $v ?>">
+                            <input type="text" class="update form-control" id="<?= $v ?>" name="<?= $v ?>" value="<?= htmlentities($row[$v]) ?>">
                             <small id="<?= $v ?>Help" class="update form-text"></small>
                             <?php }
                             endforeach; ?>
@@ -69,18 +82,17 @@ $form_data2 = [
                             <label for="<?= $v ?>" class="update_label pt-3"><?= $k ?></label>
                             <?php if ($v == 'cp_password') { ?>
                             <!-- 密碼用password type -->
-                            <input type="password" class="update form-control" id="cp_password" name="cp_password" autocomplete="new-password">
+                            <input type="password" class="update form-control" id="cp_password" name="cp_password" autocomplete="new-password" value="<?= htmlentities($row[$v]) ?>">
                             <small id="cp_passwordHelp" class="update form-text"></small>
                             <?php } else { ?>
-                            <input type="text" class="update form-control" id="<?= $v ?>" name="<?= $v ?>">
+                            <input type="text" class="update form-control" id="<?= $v ?>" name="<?= $v ?>" value="<?= htmlentities($row[$v]) ?>">
                             <small id="<?= $v ?>Help" class="update form-text"></small>
                             <?php }
                             endforeach; ?>
                         </div>
                     </div>
-
                     <div style="text-align: center">
-                        <button type="submit" class="btn btn-warning" id="submit_btn">&nbsp;確&nbsp;認&nbsp;新&nbsp;增&nbsp;</button>
+                        <button type="submit" class="btn btn-warning" id="submit_btn">&nbsp;確&nbsp;認&nbsp;修&nbsp;改&nbsp;</button>
                     </div>
                 </form>
             </div>
@@ -97,85 +109,6 @@ $form_data2 = [
 </section>
 </div>
 <script>
-    function getRandom(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
-    function rand_data() {
-        let rand_phone = "";
-        for (let i = 0; i < 8; i++) {
-            let d = getRandom(0, 9);
-            rand_phone = rand_phone + d;
-        };
-        
-        let rand_email = "";
-        for (let i = 0; i < getRandom(6, 10); i++) {
-            let c = getRandom(1, 3);
-            let a, b;
-            if (c == 1) {
-                a = getRandom(97, 122);
-                b = String.fromCharCode(a);
-            }
-            if (c == 2) {
-                a = getRandom(65, 90);
-                b = String.fromCharCode(a);
-            }
-            if (c == 3) {
-                b = getRandom(0, 9);
-            }
-            rand_email = rand_email + b;
-        };
-        
-        let rand_tax = "";
-        for (let i = 0; i < 8; i++) {
-            let d = getRandom(0, 9);
-            rand_tax = rand_tax + d;
-        };
-        let rand_stock = "";
-        for (let i = 0; i < 3; i++) {
-            let d = getRandom(0, 9);
-            rand_stock = rand_stock + d;
-        };
-        let rand_acc = "";
-        for (let i = 0; i < getRandom(6, 14); i++) {
-            let c = getRandom(1, 3);
-            let a, b;
-            if (c == 1) {
-                a = getRandom(97, 122);
-                b = String.fromCharCode(a);
-            }
-            if (c == 2) {
-                a = getRandom(65, 90);
-                b = String.fromCharCode(a);
-            }
-            if (c == 3) {
-                b = getRandom(0, 9);
-            }
-            rand_acc = rand_acc + b;
-        };
-        let rand_pass = "";
-        for (let i = 0; i < getRandom(6, 14); i++) {
-            let c = getRandom(1, 3);
-            let a, b;
-            if (c == 1) {
-                a = getRandom(97, 122);
-                b = String.fromCharCode(a);
-            }
-            if (c == 2) {
-                a = getRandom(65, 90);
-                b = String.fromCharCode(a);
-            }
-            if (c == 3) {
-                b = getRandom(0, 9);
-            }
-            rand_pass = rand_pass + b;
-        };
-        document.querySelector('#cp_phone').value = '09' + rand_phone;
-        document.querySelector('#cp_email').value = rand_email + '@gmail.com';
-        document.querySelector('#cp_tax_id').value = rand_tax;
-        document.querySelector('#cp_stock').value = rand_stock;
-        document.querySelector('#cp_account').value = rand_acc;
-        document.querySelector('#cp_password').value = rand_pass;
-    }
     let info_bar = document.querySelector('#info_bar');
     let info_position = document.querySelector('#info_position');
     const required_fields = [{
@@ -253,7 +186,7 @@ $form_data2 = [
         }
         let fd = new FormData(document.form1);
         if (isPass) {
-            fetch('CP_data_insert_api.php', {
+            fetch('CP_data_edit_api.php', {
                     method: 'POST',
                     body: fd,
                 })
@@ -266,6 +199,9 @@ $form_data2 = [
                     info_bar.innerHTML = json.info;
                     if (json.success) {
                         info_position.style.display = 'block';
+                        setTimeout(function(){
+                                location.href = 'CP_data_list.php?page=<?= $previous_page ?>';
+                        }, 1000);
                     } else {
                         info_position.style.display = 'none';
                     }
