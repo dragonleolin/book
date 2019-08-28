fd76efac42a2ff885eb88f59f7e1c79155df7f69
nana81210 <nana81210@hotmail.com>
2019-08-28 16:50:27 +0800

    nana快瘋掉

--- src/vender_books/vb_insert.php
+++ src/vender_books/vb_insert.php
@@ -23,7 +23,7 @@ $page_title = '新增出版社書籍';
         </nav>
 
         <!-- 每個人填資料的區塊 -->
-        <div class="container">
+        <div class="container2" style="margin:20px 0px 0px 0px">
             <form name="form1" onsubmit="return checkForm()">
                 <div class="d-flex">
                     <div style="min-width:700px;margin:0px 30px">
@@ -36,39 +36,39 @@ $page_title = '新增出版社書籍';
 
                         <div class="form-group">
                             <label for="name" class="update_label">書籍名稱</label>
-                            <span style="margin:0px 20px;color:red" id="isbnHelp"></span>
+                            <span style="margin:0px 20px;color:red" id="nameHelp"></span>
                             <input type="text" class="update form-control" id="name" name="name">
                         </div>
 
                         <div class="form-group">
                             <label for="author" class="update_label">作者</label>
-                            <span style="margin:0px 20px;color:red">示意:錯誤顯示訊息</span>
+                            <span style="margin:0px 20px;color:red" id="authorHelp"></span>
                             <input type="text" class="update form-control" id="author" name="author">
                         </div>
 
                         <div class="form-group">
                             <label for="publishing" class="update_label">出版社</label>
-                            <span style="margin:0px 20px;color:red">示意:錯誤顯示訊息</span>
+                            <span style="margin:0px 20px;color:red" id="publishingHelp"></span>
                             <input type="text" class="update form-control" id="publishing" name="publishing">
                         </div>
 
                         <div class="form-group">
                             <label for="publish_date" class="update_label">出版日期</label>
                             <span style="margin:0px 20px" class="my_text_blacktea_fifty">格式:2000-01-01</span>
-                            <span style="margin:0px -10px;color:red">示意:錯誤顯示訊息</span>
+                            <span style="margin:0px -10px;color:red" id="publish_dateHelp"></span>
                             <input type="text" class="update form-control" id="publish_date" name="publish_date">
                         </div>
 
                         <div class="form-group">
                             <label for="version" class="update_label">版次</label>
-                            <span style="margin:0px 20px;color:red">示意:錯誤顯示訊息</span>
+                            <span style="margin:0px 20px;color:red" id="versionHelp"></span>
                             <input type="text" class="update form-control" id="version" name="version">
                         </div>
 
                         <div class="form-group">
                             <label for="fixed_price" class="update_label">定價</label>
                             <span style="margin:0px 20px" class="my_text_blacktea_fifty">請填寫阿拉伯數字</span>
-                            <span style="margin:0px -10px;color:red">示意:錯誤顯示訊息</span>
+                            <span style="margin:0px -10px;color:red" id="fixed_priceHelp"></span>
                             <input type="text" class="update form-control" id="fixed_price" name="fixed_price">
                         </div>
                     </div>
@@ -77,14 +77,14 @@ $page_title = '新增出版社書籍';
                         <div class="form-group">
                             <label for="page" class="update_label">頁數</label>
                             <span style="margin:0px 20px" class="my_text_blacktea_fifty">請填寫阿拉伯數字</span>
-                            <span style="margin:0px -10px;color:red">示意:錯誤顯示訊息</span>
+                            <span style="margin:0px -10px;color:red" id="pageHelp"></span>
                             <input type="text" class="update form-control" id="page" name="page">
                         </div>
 
                         <div class="form-group">
                             <label for="stock" class="update_label">庫存</label>
                             <span style="margin:0px 20px" class="my_text_blacktea_fifty">請填寫阿拉伯數字</span>
-                            <span style="margin:0px -10px;color:red">示意:錯誤顯示訊息</span>
+                            <span style="margin:0px -10px;color:red" id="stockHelp"></span>
                             <input type="text" class="update form-control" id="stock" name="stock">
                         </div>
 
@@ -228,49 +228,134 @@ $page_title = '新增出版社書籍';
                         </div>
 
                         <div class="form-group">
-                            <label for="categories" class="update_label">書籍簡介</label>
+                            <label for="introduction" class="update_label">書籍簡介</label>
                             <span style="margin:0px 20px" class="my_text_blacktea_fifty">限制200字以內</span>
-                            <span style="margin:0px -10px;color:red">超過字數限制,請重新輸入</span>
+                            <span style="margin:0px -10px;color:red" id="introductionHelp"></span>
                             <textarea class="update form-control" id="introduction" rows="3"
                             style="width:700px;height:145px;resize:none"></textarea>
                         </div>
 
                         <div>
                             <button style="margin:30px 0px 0px -80px" type="submit" class="btn btn-warning" id="submit_btn">
-                            &nbsp;確&nbsp;認&nbsp;修&nbsp;改&nbsp;
+                            &nbsp;確&nbsp;認&nbsp;新&nbsp;增&nbsp;
                             </button>
                         </div>
                     </div>
-            </form>
-            
-            <div class="alert alert-primary" role="alert" id="info-bar" style="display: none"></div>  
-            <!-- 以下為修改或新增成功才會跳出來的顯示框 -->
-            <!-- <div class="success update card">
-                <div class="success card-body">
-                    <label class="success_text">修改成功</label>
-                    <div><img class="success_img" src="../../images/icon_checked.svg"></div>
-                    </div>
-            </div> -->
+            </form>                      
+        </div>
 
+        <!-- 以下為新增成功才會跳出來的顯示框 -->
+        <div class="success update card" id="success" style="display:none">
+            <div class="success card-body">
+                <label class="success_text" style="background:transparent">新增成功</label>
+                <div><img class="success_img" src="../../images/icon_checked.svg"></div>
+            </div>
+        </div>
+
+        <!-- 以下為新增失敗才會跳出來的顯示框 -->
+        <div class="success update card" id="my_false" style="box-shadow:0px 0px 10px red;display:none" >
+            <div class="success card-body">
+                <label class="success_text" style="background:transparent;color:rgb(228, 63, 63)">新增失敗,不可以偷偷來喔!</label>
+            </div>
         </div>
     </div>
-    </section>
+    
     <script>
         function checkForm() {
-            let isbn = document.querySelector('#isbn');
+            // 判斷書籍名稱,作者,出版社,版次是否有填寫
             let name = document.querySelector('#name');
             let publishing = document.querySelector('#publishing');
-            let fixed_price = document.querySelector('#fixed_price');
-            console.log(isbn);
-            
-            
+            let author = document.querySelector('#author');
+            let version = document.querySelector('#version');
+  
             let isPass = true;
             if(name.value.length<1){
                 name.style.border = '1px solid red';
-                name.closest('.nameHelp').querySelector('#nameHelp').innerText = '請填寫書籍名稱';
+                document.querySelector('#nameHelp').innerHTML = '請填寫書籍名稱';              
+                isPass = false;
+            };
+            if(publishing.value.length<1){
+                publishing.style.border = '1px solid red';
+                document.querySelector('#publishingHelp').innerHTML = '請填寫出版社名稱';              
+                isPass = false;
+            };
+            if(author.value.length<1){
+                author.style.border = '1px solid red';
+                document.querySelector('#authorHelp').innerHTML = '請填寫作者名稱';              
+                isPass = false;
+            };
+            if(version.value.length<1){
+                version.style.border = '1px solid red';
+                document.querySelector('#versionHelp').innerHTML = '請填寫版次';              
                 isPass = false;
             };
 
+
+
+            // 判斷isbn碼,出版日期,定價,頁數,庫存,書籍簡介格式是否正確                       
+            let s, item;
+
+            const required_fields = [
+                {
+                    id: 'isbn',
+                    pattern: /(^\d{10}$)|(^\d{13}$)/,
+                    info: '請填寫正確的isbn碼'
+                },
+                {
+                    id: 'publish_date',
+                    pattern: /^\d{4}\-?\d{2}\-?\d{2}$/,
+                    info: '請填寫正確的出版日期格式'
+                },
+                {
+                    id: 'fixed_price',
+                    pattern: /^\d+$/,
+                    info: '請填寫正確的定價格式'
+                },
+                {
+                    id: 'page',
+                    pattern: /^\d+$/,
+                    info: '請填寫正確的頁數格式'
+                },
+                {
+                    id: 'stock',
+                    pattern: /^\d+$/,
+                    info: '請填寫正確的庫存格式'
+                },
+                {
+                    id: 'introduction',
+                    pattern: /^\S{1,200}$/,
+                    info: '超過字數限制,請重新輸入'
+                },
+            ];
+
+            // 拿到對應的 input element (el), 顯示訊息的 small element (infoEl)
+            for(s in required_fields){
+            item = required_fields[s];
+            item.el = document.querySelector('#' + item.id);
+            item.infoEl = document.querySelector('#' + item.id + 'Help');
+            };
+
+             // 先讓所有欄位外觀回復到原本的狀態
+            for(s in required_fields) {
+                item = required_fields[s];
+                item.el.style.border = '1px solid #CCCCCC';
+                item.infoEl.innerHTML = '';
+            };
+
+            for(s in required_fields) {
+            item = required_fields[s];
+                if(! item.pattern.test(item.el.value)){
+                    item.el.style.border = '1px solid red';
+                    item.infoEl.innerHTML = item.info;
+                    isPass = false;
+                };
+            };
+
+            // 全部都正確送出表單到後台
+            let container2 =  document.querySelector('.container2');
+            let success = document.querySelector('#success');
+            let my_false = document.querySelector('#my_false');
+            
             if(isPass){
             let fd = new FormData(document.form1);
             fetch('vb_data_insert_api.php',{
@@ -281,70 +366,29 @@ $page_title = '新增出版社書籍';
             .then(response=>{
                 return response.json();
             })
-
+            // 收到後台回傳的新增判斷(是否成功,然後顯示到前台讓用戶知道)
+            // 新增成功跳回出版社書籍的datalist,失敗就回到上一層
             .then(json=>{
                 console.log(json);
-                info_bar.style.display = 'block';
-                info_bar.innerHTML = json.info;
                 if(json.success){
-                    info_bar.className = 'alert alert-success';
-                    card.style.display = 'none';
+                    mysuccess.style.display = 'block';
+                    container2.style.display = 'none';
+                    setTimeout(function(){
+                        location.href = 'vb_data_list.php';
+                    },1000)
                 } else {
-                    info_bar.className = 'alert alert-danger';
-                    card.style.display = 'block';
+                    myfalse.style.display = 'block';
+                    container2.style.display = 'none';
+                    setTimeout(function(){
+                        location.href = document.referrer;
+                    },1000)
                 }
             });
-        }
-
-            
-            // let error_text = document.querySelectorAll('.error_text');
-            // let error_text = document.querySelectorAll('.error_text');
-            // console.log(error_text);
-            // let i, s, item;
-
-            // const required_fields = [
-            //     {
-            //         id: 'isbn',
-            //         pattern: /(^\d{10}$)|(^\d{13}$)/,
-            //         info: '請填寫正確的isbn碼'
-            //     },
-            //     {
-            //         id: 'publish_date',
-            //         pattern: /^\d{4}\-?\d{2}\-?\d{2}$/,
-            //         info: '請填寫正確的出版日期格式'
-            //     },
-            //     {
-            //         id: 'fixed_price',
-            //         pattern: /^\d$/,
-            //         info: '請填寫正確的定價格式'
-            //     },
-            //     {
-            //         id: 'page',
-            //         pattern: /^\d$/,
-            //         info: '請填寫正確的頁數格式'
-            //     },
-            //     {
-            //         id: 'stock',
-            //         pattern: /^\d$/,
-            //         info: '請填寫正確的庫存格式'
-            //     },
-            // ];
-
-        // 拿到對應的 input element (el), 顯示訊息的 small element (infoEl)
-
-            // for(s in required_fields){
-            // item = required_fields[s];
-            // item.el = document.querySelector('#' + item.id);
-            // item.infoEl = document.querySelector('#' + item.id + 'Help');
-            // };
-            
-
+        }            
            
             //不讓表單直接送出
             return false;
         }
-
-
     </script>
 
     <?php include __DIR__ . '/../../pbook_index/__html_foot.php' ?>
