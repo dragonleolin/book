9954fbed15f3b97f118f1d8f566ea0bffd2acf4d
chiangmengting <53255886+chiangmengting@users.noreply.github.com>
2019-09-01 16:42:06 +0800

    123

--- src/venderBooks_Management/vb_data_insert.php
+++ src/venderBooks_Management/vb_data_insert.php
@@ -65,7 +65,7 @@ $totalPages = ceil($totalRows / $per_page); //取得總頁數
                         <label for="isbn" class="update_label">・ISBN</label>
                         <span style="margin:0px 20px" class="my_text_blacktea_fifty">阿拉伯數字10碼或13碼</span>
                         <span style="margin:0px -10px;color:red" id="isbnHelp"></span>
-                        <input type="text" class="update form-control" id="isbn" name="isbn">
+                        <input type="text" class="update form-control" id="isbn" name="isbn" onchange="isbn_test()">
                     </div>
 
                     <div class="form-group">
@@ -132,7 +132,7 @@ $totalPages = ceil($totalRows / $per_page); //取得總頁數
                             </button>
                         </div>
                         <div style="height: 230px;width: 230px;border: 1px solid #ddd">
-                            <img style="object-fit: contain;width: 100%;height: 100%" id="demo"/>
+                            <img style="object-fit: contain;width: 100%;height: 100%" id="demo" />
                         </div>
                     </div>
 
@@ -199,6 +199,46 @@ $totalPages = ceil($totalRows / $per_page); //取得總頁數
         reader.readAsDataURL(file);
     });
 
+
+    function isbn_test() {
+        let isbn = document.querySelector('#isbn').value;
+        let test = 0;
+        let isPass = false;
+        isbn = isbn.trim();
+
+        if (isbn.length == 10) {
+            for (let i = 0; i < 9; i++) {
+                test += isbn[i] * (10 - i);
+            }
+            test = 11 - test % 11;
+            test = test == 10 ? 'X' : test;
+            isPass = isbn[9] == test ? 'true' : 'false';
+        } else if (isbn.length == 13) {
+            for (let i = 0; i < 12; i++) {
+                let weighting = (i % 2) ? 3 : 1;
+                test += isbn[i] * weighting;
+            }
+
+            test = 10 - test % 10;
+            isPass = isbn[12] == test ? 'true' : 'false';
+        } else if (isbn.length == 8) {
+            for (let i = 0; i < 7; i++) {
+                test += isbn[i] * (8 - i);
+            }
+            test = 11 - test % 11;
+            test = test == 10 ? 'X' : test;
+            isPass = isbn[7] == test ? 'true' : 'false';
+        } else {
+            console.log('格式錯誤');
+        }
+        setTimeout(function() {
+            document.querySelector('#isbnHelp').innerHTML = '請填寫正確isbn碼格式';
+        }, 2000)
+
+        return isPass;
+    }
+
+
     function checkForm() {
         // 判斷書籍名稱,作者,出版社,版次是否有填寫
         let name = document.querySelector('#name');
@@ -207,6 +247,8 @@ $totalPages = ceil($totalRows / $per_page); //取得總頁數
         let version = document.querySelector('#version');
 
         let isPass = true;
+        isPass = isbn_test();
+
         if (name.value.length < 1) {
             name.style.border = '1px solid red';
             document.querySelector('#nameHelp').innerHTML = '請填寫書籍名稱';
@@ -246,7 +288,6 @@ $totalPages = ceil($totalRows / $per_page); //取得總頁數
             document.querySelector('#versionHelp').innerHTML = '';
             isPass = true;
         };
-
 
 
         // 判斷isbn碼,出版日期,定價,頁數,庫存,書籍簡介格式是否正確                       
