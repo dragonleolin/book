ecb172fa92d7d5da03fdb9542889ebfaa939a86f
dragonleolin <wei.che0812@gmail.com>
2019-09-02 00:08:17 +0800

    connect更正

--- pbook_index/__connect_db.php
+++ pbook_index/__connect_db.php
@@ -1,14 +1,11 @@
 <?php
 
-// $db_host = '192.168.27.186';
-// $db_name = 'pbook';
-// $db_user = 'weihsun';
-// $db_pass = '841012';
-
-$db_host = 'localhost';
+$db_host = '192.168.27.186';
 $db_name = 'pbook';
-$db_user = 'root';
-$db_pass = 'root';
+$db_user = 'weihsun';
+$db_pass = '841012';
+
+
 
 $dsn = "mysql:host={$db_host};dbname={$db_name}";
 
