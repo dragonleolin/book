2314474dd91bd0551d25d68653e68a0bf7b6d493
luke <weu.che0812@gmail.com>
2019-09-02 21:17:56 +0800

    新增裡驗證會員編號

---
+++ src/memberBooks_Management/MB_searchMember_api.php
@@ -0,0 +1,42 @@
+<?php
+
+require __DIR__ . '/__admin_required.php';
+require __DIR__ . '/__connect_db.php';
+
+
+
+$result = [
+    'success'=> false,
+    'code' => 400,
+    'info' => '沒有輸入會員編號',
+];
+
+
+if(empty($_POST['mb_shelveMember'])){
+    echo json_encode($result, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+// echo $_POST['mb_shelveMember'];
+$sql = "SELECT `MR_number` FROM `mr_information` WHERE MR_number = '".$_POST['mb_shelveMember']."'";
+
+
+$stmt =  $pdo->query($sql)->fetchAll();
+
+// var_dump($stmt);
+// var_dump(count($stmt)>0);
+// exit;
+
+
+
+if(count($stmt)>0){
+    $result['success'] = true;
+    $result['code'] = 200;
+    $result['info'] = '查到有會員編號';
+}else{
+    $result['code'] = 420;
+    $result['info'] = '沒有此會員編號';
+    $result['error'] = $stmt;
+}
+
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
