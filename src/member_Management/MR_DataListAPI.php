1a5d83ab61f9f4573bb605c6e640837d3f00e6c2
Ruby <ruby0771134@gmail.com>
2019-09-23 13:30:13 +0800

    datalist

--- src/member_Management/MR_DataListAPI.php
+++ src/member_Management/MR_DataListAPI.php
@@ -1,10 +1,9 @@
 <?php require  'MR_db_connect.php' ?>
 
 <?php
-
-$page = isset($_POST['page']) ? intval($_POST['page']) : 1;
-$per_page = isset($_POST['per_page']) ? intval($_POST['per_page']) : 10; // 每頁顯示的筆數
-$params = [];
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$per_page = isset($_GET['per_page']) ? intval($_GET['per_page']) : 10; // 每頁顯示的筆數
 $count = "SELECT COUNT(1) FROM `mr_information`"; //用count計算出總筆數
 $totalRows = $pdo->query($count)->fetch(PDO::FETCH_NUM)[0];
 // echo "$totalRows <br>";
@@ -21,12 +20,14 @@ $totalPage = ceil($totalRows / $per_page); //ceil()無條件進位
 //     exit;
 // }
 
+// http_build_query($params)
 
 $sql = "SELECT * FROM `mr_information` ORDER BY `sid` LIMIT " . ($page - 1) * $per_page . "," . $per_page;
 $stmt = $pdo->query($sql)->fetchAll();
 $result = [
     'totalPage' => $totalPage,
     'page' => $page,
+    'search' => $search,
     'totalRows' => $totalRows,
     'rows' => $stmt,
 ];
